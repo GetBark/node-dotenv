480c7340fbc5756ea5c295012f47eb107870dae2
Louis Capitanchik <contact@louiscap.co>
2018-02-06 11:32:48 +0000

    Publically expose file IO methods to load env data

--- dotenv.js
+++ dotenv.js
@@ -1,3 +1,23 @@
+const fs = require('./lib/fs')
+const proc = require('./lib/process')
 const reader = require('./lib/reader')
 
-exports.reader = reader
+function readSyncAndApply(opts = {}) {
+	const contents = fs.readSync(opts.path, opts.encoding)
+	proc(contents)
+}
+function readAsyncAndApply(opts = {}) {
+	return fs.readAsync(opts.path, opts.encoding)
+		.then(file => proc(file))
+		.then(() => true)
+}
+
+module.exports = {
+	config: readSyncAndApply,
+	load: readSyncAndApply,
+	configAsync: readAsyncAndApply,
+	loadAsync: readAsyncAndApply,
+
+	parse: reader,
+	putenv: proc,
+}
